45b8b48e3eb6db72fb314b23c501a36660153a35
Heshko <shooter.metall@gmail.com>
2019-09-23 09:42:31 +0200

    add login adn initial menu

--- Program.cs
+++ Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-
+using static System.Console;
+using System.Threading;
 namespace ATM
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isInvaidCard = true;
+            while (isInvaidCard)
+            {
+                Clear();
+                Write("Card number: ");
+
+                string cardNumber = ReadLine();
+
+                Write("Pin code: ");
+
+                string pinCode = ReadLine();
+
+                if (cardNumber == "123456789" && pinCode == "1234")
+                {
+                    isInvaidCard = false;
+                }
+                else
+                {
+                    WriteLine("Invalid card");
+
+                    Thread.Sleep(2000);
+                }
+            }
+
+            WriteLine("1. withdraw");
+            WriteLine("2. View details");
+            WriteLine("3. Exit");
+            ConsoleKeyInfo keyPressed = ReadKey();
+            switch (keyPressed.Key)
+            {
+             
+                case ConsoleKey.D1:
+                    break;
+                case ConsoleKey.D2:
+                    break;
+                case ConsoleKey.D3:
+                    break;
+                
+                
+            }
         }
     }
 }
