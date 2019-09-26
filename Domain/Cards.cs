a9c04f5934014fc7738d458f7af59ad9322dffcf
Heshko <shooter.metall@gmail.com>
2019-09-26 09:35:25 +0200

    small change

---
+++ Domain/Cards.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM.Domain
+{
+    class Cards
+    {
+        string  cardNumber;
+
+        string pinCode;
+
+        public Cards(string cardNumber, string pinCode)
+        {
+            this.cardNumber = cardNumber;
+
+            this.pinCode = pinCode;
+        }
+        //public string GetCardNumber()
+        //{
+        //    return cardNumber;
+        //}
+        public string CardNumber
+        {
+            get { return cardNumber; }
+          //  set { cardNumber = value; }
+        }
+        //public string GetPinCord()
+        //{
+        //    return pinCode;
+        //}
+        public string PinCode
+        {
+            get { return pinCode; }
+            set {
+                if (value == pinCode)
+                {
+                    throw new ArgumentException("new pincode cannot be same as previous");
+                }
+
+                pinCode = value; }
+        }
+
+        public void setPinCode(string value)
+        {
+            if (value == pinCode)
+            {
+                throw new ArgumentException("new pincode cannot be same as previous");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using static System.Console;
 using System.Threading;
+using ATM.Domain;
 namespace ATM
 {
     class Program
     {
+       static Cards[] cards = new Cards[100];
         static void Main(string[] args)
         {
-            bool isInvalidCard = true;
+            bool isValidCard = false;
+           
+
+            cards[0] = new Cards(cardNumber:"123", pinCode:"123");
+            cards[1] = new Cards(cardNumber:"1234213", pinCode:"123453123");
+
+            //Cards card = new Cards("12345","12345");
            
             while (true)
             {
@@ -22,31 +30,23 @@ namespace ATM
 
                 Clear();
 
-                if (cardNumber == "123456789" && pinCode == "1234")
+                Cards vaildCard = FindValidCard(cardNumber,pinCode);
+
+                for(int x = 0; x < cards.Length; x++)
                 {
-                isInvalidCard = false;
-                    while (!isInvalidCard)
+                    if (cards[x] == null) continue;
+                     if ( cards[x].CardNumber==cardNumber || cards[x].PinCode == pinCode)
                     {
-                       
-                        WriteLine("1. withdraw");
-                        WriteLine("2. View details");
-                        WriteLine("3. Abort");
-                        ConsoleKeyInfo keyPressed = ReadKey();
-                        switch (keyPressed.Key)
-                        {
-
-                            case ConsoleKey.D1:
-
-                                break;
-                            case ConsoleKey.D2:
-                                break;
-                            case ConsoleKey.D3:
-                                break;
-                        }
-                  
-
-
+                        vaildCard = cards[x];
+                        break;
                     }
+                }
+
+
+                if (vaildCard!=null)
+                {
+                    isValidCard = true;
+                  
                 }
                 else
                 {
@@ -54,9 +54,41 @@ namespace ATM
 
                     Thread.Sleep(2000);
                 }
+
+                while (isValidCard)
+                {
+
+                    WriteLine("1. withdraw");
+                    WriteLine("2. View details");
+                    WriteLine("3. Abort");
+                    ConsoleKeyInfo keyPressed = ReadKey(true);
+                    switch (keyPressed.Key)
+                    {
+
+                        case ConsoleKey.D3:
+                            isValidCard = false;
+                            break;
+                    }
+
+
+
+                }
             }
           
           
         }
-    }
+        static Cards FindValidCard(string cardNumber, string pinCode)
+        {
+            for (int i = 0; i < cards.Length; i++)
+            {
+                if (cards[i] == null) continue;
+                if (cards[i].CardNumber == cardNumber && cards[i].PinCode == pinCode)
+                {
+                    return  cards[i];
+                 
+                }
+            }
+            return null;
+        }
+}
 }
